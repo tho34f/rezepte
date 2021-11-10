53f9f6fdd93012414fc7603f013ac53ab7e2ccbf
tho34f <77640449+tho34f@users.noreply.github.com>
2021-11-10 07:34:46 +0100

    Anpassungen

--- Konstanten.py
+++ Konstanten.py
@@ -11,11 +11,8 @@ favIcon = 'D:/python/Rezepte/thorben.ico'
 rezeptIcon = 'D:/python/Rezepte/rezepte.jpg'
 
 # Texte für Lables, Buttons etc.
-opiningLabel = "Herzlich willkommen bei dieser Anwendung. Diese bietet die Möglichkeit, ein Rezept aus der Dropbox downzuloaden.\n Bitte geben Sie eine Kategorie ein. \n 1)Backen  2)Brotaufstriche  3)Eintöpfe  4)Gemüse  5)Hauptgerichte \n 6)Partyrezepte  7)Salate"
+openingLabel = "Herzlich willkommen bei dieser Anwendung. Diese bietet die Möglichkeit, ein Rezept aus der Dropbox downzuloaden.\n Bitte geben Sie eine Kategorie ein: \n"
 
 categorieNotFound = "Die eingegebene Kategorie konnte nicht gefunden werden. Bitte eine andere Kategorie eingeben!"
 searchRezept = "Bitte Geben Sie ein Rezept ein."
 uploadRezept = "Bitte Geben Sie ein Rezept für den Upload ein."
-
-# Sonsite Konstanten
-kategorien = ['Backen', 'Brotaufstriche', 'Eintöpfe', 'Gemüse', 'Hauptgerichte', 'Partyrezepte', 'Salate']
--- RezepteService.py
+++ RezepteService.py
@@ -5,13 +5,20 @@ Created on Wed Oct 13 19:09:20 2021
 @author: thorb
 """
 
-import Konstanten
+kategorien = []
+
+
+def fillKategorie(db):
+    kategorien.clear()
+    response = db.files_list_folder('')
+    for file in response.entries:
+        kategorien.append(file.name)
 
 
 def check_kategorie(gui):
     """ Gewählte Kategorie prüfen """
     kategorieName = gui.kategorieNameInput.get()
-    for kategorie in Konstanten.kategorien:
+    for kategorie in kategorien:
         if kategorieName == kategorie:
             return True
     return False
