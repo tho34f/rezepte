006ccb469396cc20b34c6dcbdcb6ec6288173c13
tho34f <77640449+tho34f@users.noreply.github.com>
2021-11-11 07:25:08 +0100

    Add files via upload

--- Konstanten.py
+++ Konstanten.py
@@ -5,10 +5,12 @@ Created on Fri Apr 30 07:20:18 2021
 @author: thorb
 """
 
+from pathlib import Path
+
 # dropbox-konstanten
 droboauth = 'kU34bXLSJfMAAAAAAAAAARLRIV8PFo_6IvKhSysNlTXO9mSDln6fUL-4eseEKmML'
-favIcon = 'D:/python/Rezepte/thorben.ico'
-rezeptIcon = 'D:/python/Rezepte/rezepte.jpg'
+favIcon = Path(__file__).with_name("thorben.ico")
+rezeptIcon = Path(__file__).with_name("rezepte.jpg")
 
 # Texte für Lables, Buttons etc.
 openingLabel = "Herzlich willkommen bei dieser Anwendung. Diese bietet die Möglichkeit, ein Rezept aus der Dropbox downzuloaden.\n Bitte geben Sie eine Kategorie ein: \n"
